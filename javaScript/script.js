b616bf657866193165afbca1bcfd35d254893e80
Kevin-alcaino <wthblck1@gmail.com>
2026-06-27 19:12:24 -0400

    se edita el codigo javaScript

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,14 +1,19 @@
+
 function restarclics() {
   var cantidad = document.querySelector("#cantidad");
   var actual = cantidad.innerHTML;
-  var menos = (cantidad) - 1
-
-  cantidad.innerHTML = menos
+  var menos = actual - 1
+  if (actual > 0)
+  cantidad.innerHTML = menos;
 }
 
 function sumarclics() {
     var cantidad = document.querySelector("#cantidad");
+    var precio = document.querySelector("#precio")
     var actual = cantidad.innerHTML;
-    var mas = (cantidad) + 1
-    cantidad.innerHTML = mas
+    var mas = (actual -0) + 1
+    cantidad.innerHTML = mas;
+    var total = mas * 16990;
+    precio.innerHTML = "$" + total;
 }
+
